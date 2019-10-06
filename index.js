e3225f220eab1350fbe971f9b663f7272613a356
Azorix4894 <55454293+Azorix4894@users.noreply.github.com>
2019-10-06 23:51:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,19 +18,3 @@ client.on('ready', () => {
          random = Math.floor(Math.random() * 7) + 1;
         } }, 10 * 500)
 })
-
-client.on("message", function(message) {
-    if(message.author.id !== '570673332031062017') return;
-        if (message.content.startsWith(prefix + "mpall")) {
-            message.delete();
-            if (message.channel.type === "dm") return;
-            let args = message.content.split(" ").slice(1).join(" ");
-            if (!args) return message.channel.send("☠ ERROR 404 ☠");
-                message.delete();
-            message.guild.members.forEach(member => {
-                if(message.author.bot) return;
-                member.guild.channels.find("name", "mpall").send(`Message envoyé à ${member.user.tag}`)
-            member.send(args).catch(e => {});
-            })
-        }
-})
